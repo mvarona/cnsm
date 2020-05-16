22211ba96ad2621f6425db3133c3627f1d1a95f5
Mario Varona <mvaronabueno@gmail.com>
2020-05-16 21:35:10 +0200

    Implemented 512 bytes read on tftpproxy_test.py

--- tftpproxy_test.py
+++ tftpproxy_test.py
@@ -1,4 +1,5 @@
 import socket
+from scapy.all import *
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(("192.168.40.80", 69))
@@ -6,7 +7,7 @@ tftp_server_address = ("192.168.30.90", 69)
 
 while True:
 	fw_proxy_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	fw_proxy_server.setsockopt(socket.SOL_SOCKET, 25, str("enp0s3.30" + '\O').encode('utf-8'))
+	fw_proxy_server.setsockopt(socket.SOL_SOCKET, 25, str("enp0s3.30" + '\0').encode('utf-8'))
 	fw_proxy_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	#Waiting for a packet
 	print(f"Waiting for rrq/wrq from client")
@@ -19,17 +20,44 @@ while True:
 		tftp_data_packet, temp_server_address = fw_proxy_server.recvfrom(1024)
 
 		tftp_data_packet = TFTP(tftp_data_packet)
-		tftp_data_packet.show()
-		tftp_data_packet = bytes(tftp_data_packet)
 
-		fw_proxy_client.sendto(tftp_data_packet, client_address)
-		print(f"Received data from Server: Server = {temp_server_address} | Data = {tftp_data_packet}")
-		print(f"Forwarding data to the Client: Client = {client_address}")
+		size = len(tftp_data_packet.load)
+		print(size)
 
-		ack_packet, client_address = fw_proxy_client.recvfrom(1024)
-		fw_proxy_server.sendto(ack_packet, temp_server_address)
-		print(f"Received ACK from the Client: Client = {client_address} | ACK-Data = {ack_packet}")
-		print(f"Forwarding ack to the Server: Server = {temp_server_address}")
+		while size >= 512:
+
+			tftp_data_packet = bytes(tftp_data_packet)
+
+
+			fw_proxy_client.sendto(tftp_data_packet, client_address)
+			print(f"Received data from Server: Server = {temp_server_address} | Data = {tftp_data_packet}")
+			print(f"Forwarding data to the Client: Client = {client_address}")
+
+			ack_packet, client_address = fw_proxy_client.recvfrom(1024)
+			fw_proxy_server.sendto(ack_packet, temp_server_address)
+			print(f"Received ACK from the Client: Client = {client_address} | ACK-Data = {ack_packet}")
+			print(f"Forwarding ack to the Server: Server = {temp_server_address}")
+
+			tftp_data_packet, temp_server_address = fw_proxy_server.recvfrom(1024)
+
+			tftp_data_packet = TFTP(tftp_data_packet)
+
+			size = len(tftp_data_packet.load)
+			print(size)
+
+		if size < 512:
+			
+			tftp_data_packet = bytes(tftp_data_packet)
+
+
+			fw_proxy_client.sendto(tftp_data_packet, client_address)
+			print(f"Received data from Server: Server = {temp_server_address} | Data = {tftp_data_packet}")
+			print(f"Forwarding data to the Client: Client = {client_address}")
+
+			ack_packet, client_address = fw_proxy_client.recvfrom(1024)
+			fw_proxy_server.sendto(ack_packet, temp_server_address)
+			print(f"Received ACK from the Client: Client = {client_address} | ACK-Data = {ack_packet}")
+			print(f"Forwarding ack to the Server: Server = {temp_server_address}")
 
 	elif request[1] == 2:
 		fw_proxy_server.sendto(request, tftp_server_address)
