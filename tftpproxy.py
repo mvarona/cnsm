33d5cf035d1d208ac59875ecb2a940ddc504bd37
mvb <mvaronabueno@gmail.com>
2020-06-02 19:31:22 +0200

    Attemot v to fix bug on error scenario 2 and 5 on TFTP

--- tftpproxy.py
+++ tftpproxy.py
@@ -384,7 +384,7 @@ while True:
 		fw_proxy_client.sendto(ack_server_mod_bytes, client_address)
 		print(f"Forwarding ack/error to the Client: Client = {client_address}")
 
-		if not (chosenAttack == ATTACK_FILE_NOT_FOUND or chosenAttack == ATTACK_ACCESS_VIOLATION or chosenAttack == ATTACK_FILE_NOT_FOUND_WRQ):
+		if not (chosenAttack == ATTACK_ACCESS_VIOLATION or chosenAttack == ATTACK_FILE_NOT_FOUND_WRQ):
 
 			datapacket, clientaddress = fw_proxy_client.recvfrom(BUFFER_TFTP)
 
